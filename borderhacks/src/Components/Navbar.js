4be5dee6a1cf17a1dcf7871c875cd2224cd110b2
Williaml9329 <62862045+Williaml9329@users.noreply.github.com>
2021-09-25 15:30:29 -0400

    commit

--- borderhacks/src/Components/Navbar.js
+++ borderhacks/src/Components/Navbar.js
@@ -5,10 +5,6 @@ import React from "react"
             <div className = "navDiv"> 
                 <nav className = "navBar"> 
                     <button className = "navTitle"> <strong>Project Name</strong> </button>
-                    <button className = "navButtons"> B1 </button>
-                    <button className = "navButtons"> B2 </button>
-                    <button className = "navButtons"> B3 </button>
-                    <button className = "navButtons"> B4 </button>
                 </nav>
             </div>
         )
