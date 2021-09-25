4ea2b94d71938fab25abc3c193702922adb8dd7a
Williaml9329 <62862045+Williaml9329@users.noreply.github.com>
2021-09-25 17:31:37 -0400

    commit

--- borderhacks/src/App.js
+++ borderhacks/src/App.js
@@ -2,15 +2,18 @@ import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Home from "./Components/Home/Home"
 import Traffic from "./Components/Traffic/Traffic"
+import HomePage from "./Components/Home/Home"
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path = "/HomePage" component = {Home} />
-        <Route exact path = "/Traffic" component = {Traffic} />
-      </Switch>
-    </Router>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path = "/HomePage" component = {Home} />
+          <Route exact path = "/Traffic" component = {Traffic} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- borderhacks/src/Components/Traffic/Traffic.js
+++ borderhacks/src/Components/Traffic/Traffic.js
@@ -1,9 +1,41 @@
-import React from "react"
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import "./Traffic.css"
 
-function Traffic() {
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 42.317432,
+      lng: -83.026772
+    },
+    zoom: 11
+  };
+
+  render() {
     return (
-        <p> </p>
-    )
+      // Important! Always set the container height explicitly
+        <div className = "TrafficContainer">
+            <div className = "map" style={{ height: '100vh', width: "70%" }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: "AIzaSyC2-HdSCQjV2HdgkF6iQdvdcBYm7bBF39w" }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
+                <AnyReactComponent
+                    lat={42.317432}
+                    lng={-83.026772}
+                    text="Windsor"
+                />
+                </GoogleMapReact>
+            </div>
+            <div className = "TrafficInfo">
+                <center><h2 className = "TrafficInfoTitle"> Traffic Analysis </h2></center>
+            </div>
+        </div>
+    );
+  }
 }
 
-export default Traffic
+export default SimpleMap;
